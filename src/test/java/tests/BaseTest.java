ed7e70ec8e36fbfc49ee3964aa8ef3eaa44c8dc4
evg <evg73ul@mail.ru>
2023-04-05 12:46:35 +0400

    change v

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -21,6 +21,7 @@ public class BaseTest {
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+        options.setCapability("chrome.version", "111");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
